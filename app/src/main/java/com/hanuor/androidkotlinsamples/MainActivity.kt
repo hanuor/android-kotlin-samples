9c7d1260259da31d5d9377fade8732fe69bda030
hanuor <hanuortech@gmail.com>
2017-05-18 22:01:29 +0530

    Tv

--- app/src/main/java/com/hanuor/androidkotlinsamples/MainActivity.kt
+++ app/src/main/java/com/hanuor/androidkotlinsamples/MainActivity.kt
@@ -2,12 +2,14 @@ package com.hanuor.androidkotlinsamples
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val helloWorld = findViewById(R.id.helloWorld) as TextView
+        helloWorld.setText("Hey world!. This is Kotlin!")
     }
 }
